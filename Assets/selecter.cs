da8966866cda12be59c46d910217ca29fbe5a752
Parker McCoog <parkermccoog@Parkers-MacBook-Pro.local>
2024-02-16 11:07:56 -0800

    Elliot told me to again

--- Assets/selecter.cs
+++ Assets/selecter.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WakeUp : MonoBehaviour
+public class selector : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
